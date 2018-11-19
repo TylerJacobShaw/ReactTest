af6fa37b15b1abfaeb191d70b538d6583bf8d627
Tyler Shaw <tylerjacobshaw@gmail.com>
2018-11-18 21:41:05 -0600

    git init, remade index page with react components. Replaced contents of Index.cshtml page with div's to render react components too. Added jumbotron-page.js, more-libraries-page.js, web-hosting-page.js, and getting-started-page.js components to scripts folder.

---
+++ ReactWeb/Scripts/getting-started-page.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+class GettingStarted extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+}
+const gettingStartedPage = document.querySelector('#gettingStarted').innerHTML = '    <div class="col-md-4"> <div id="like_button_container"></div> <h2>Getting started</h2><p> ASP.NET MVC gives you a powerful, patterns-based way to build dynamic websites that enables a clean separation of concerns and gives you full control over markup for enjoyable, agile development. </p> <p><a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301865">Learn more &raquo;</a></p> </div>';
+ReactDOM.render(React.createElement.GettingStarted, gettingStartedPage);
---
+++ ReactWeb/Scripts/jumbotron-page.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+class JumboTron extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+}
+const domContainer = document.querySelector('#jumbotron').innerHTML = '<h1>ASP.NET</h1><p class="lead"> ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS and JavaScript.</p ><p><a href="https://asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>';
+ReactDOM.render(React.createElement.JumboTron, domContainer);
